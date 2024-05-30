23be3d4893e02902dd61270711678c97d7b4ffc3
Midori <natsukaze.midori@gmail.com>
2024-05-30 14:30:05 +0900

    Add DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:npm/app.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
+void main() => runApp(
+      ProviderScope(
+        child: DevicePreview(
+          enabled: kIsWeb,
+          builder: (_) => const App(),
+        ),
+      ),
+    );
